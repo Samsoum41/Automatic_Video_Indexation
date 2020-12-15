c8898a0bd900f3a7422db1d4eaf396c9b6171b66
tzeller97 <torbenzeller@gmx.de>
2020-12-15 19:04:30 +0100

    restructuration2

---
+++ token.js
@@ -0,0 +1,27 @@
+$(function() {
+	var params = {
+		// Request parameters
+		location: "trial",
+		accountId: "1a206862-1b40-440c-9057-4a6c2bc9b364",
+		allowEdit: "true",
+	}; 
+
+	$.ajax({
+		url: "https://api.videoindexer.ai/Auth/"+params.location+"/Accounts/"+params.accountId+"/AccessToken?allowEdit="+params.allowEdit,
+		beforeSend: function(xhrObj){
+			// Request headers
+			xhrObj.setRequestHeader("x-ms-client-request-id","");
+			xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","fc9726bc506c420f8510a57ea140dbf6");
+		},
+		type: "GET",
+		// Request body
+		data: "{body}"
+	})
+		.done(function(data) {
+			alert("success get token " +data);
+		})
+		.fail(function() {
+			alert("error get token");
+		});
+});
+
---
+++ upload.js
@@ -0,0 +1,28 @@
+$(function() {
+	var params = {
+		location: "trial",
+		accountId: "1a206862-1b40-440c-9057-4a6c2bc9b364",
+		name: prompt("Please enter the name of the video"),
+		videoUrl: prompt("Please enter the url of the video"), 
+		token: prompt("Please enter your account acces token") 
+	};
+
+	$.ajax({
+		url: "https://api.videoindexer.ai/"+params.location+"/Accounts/"+params.accountId+"/Videos?name="+params.name+"&privacy=Private&videoUrl="+params.videoUrl+"&indexingPreset=Default&streamingPreset=Default&sendSuccessEmail=False&accessToken="+params.token,
+		beforeSend: function(xhrObj){
+			// Request headers
+			xhrObj.setRequestHeader("x-ms-client-request-id","");
+			xhrObj.setRequestHeader("Content-Type","multipart/form-data");
+			xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","fc9726bc506c420f8510a57ea140dbf6");
+		},
+		type: "POST",
+		// Request body
+		data: "{body}"
+	})
+		.done(function(data) {
+			alert("success ");
+		})
+		.fail(function() {
+			alert("error");
+		});
+});
